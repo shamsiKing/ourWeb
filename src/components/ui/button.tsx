78d103aec70873089ac2a0dd1a079979e1d3262f
shamsiKing <shamsiking00@gmail.com>
2025-07-31 18:28:09 +0500

    fixed

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -33,7 +33,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
@@ -43,9 +43,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -53,35 +53,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
-  <img
-            src="	https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a40a333feb916cb51b_Section%201.jpg"
-            alt="img1"
-          />
-          <img
-            src="	https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a4a89e7792c609c1a0_Section%206.jpg"
-            alt="img2"
-          />
-          <img
-            src="	https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a4f34075a836e8ed14_Section%203.jpg"
-            alt="img3"
-          />
-          <img
-            src="	https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a46c6544cb819a8d81_Section%204.jpg"
-            alt="img4"
-          />
-          <img
-            src="https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a43dec31b372f4ddcb_Section%205.jpg"
-            alt="img5"
-          />
-          <img
-            src="https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a43dec31b372f4ddcb_Section%205.jpg"
-            alt="img6"
-          />
-          <img
-            src="https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/674568a40a333feb916cb49c_Section%207.jpg"
-            alt=""
-          />
+export { Button, buttonVariants };
