a9007ba4641da6b94381edeb52169bda2ad41827
aziza12555 <azizajoraboyeva75@gmail.com>
2025-08-01 16:11:47 +0500

    ourweb

--- src/components/pages/innerPage.tsx
+++ src/components/pages/innerPage.tsx
@@ -7,14 +7,20 @@ const InnerPage = () => {
     <div className="w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-10 py-20 space-y-40">
         <div className="text-center justify-center items-center space-y-5">
-          <Button className="rounded-3xl w-50 bg-[#3b4eff] gap-2 px-6 py-4">
-            <img
-              src="https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/67454e5d6398e79b4ca5a8b5_Webflow%20Icon.png"
-              alt="Webflow Icon"
-              className=""
-            />
-            <h1 className="text-lg text-white">Inner Pages</h1>
-          </Button>
+      <a
+  href="https://vidion-template.webflow.io/home/home-one"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <Button className="rounded-3xl w-50 bg-[#3b4eff] gap-2 px-6 py-4">
+    <img
+      src="https://cdn.prod.website-files.com/672aef24c0531e325c1a619e/67454e5d6398e79b4ca5a8b5_Webflow%20Icon.png"
+      alt="Webflow Icon"
+    />
+    <h1 className="text-lg text-white">Inner Pages</h1>
+  </Button>
+</a>
+
           <h2 className="text-4xl font-medium">
             Innovative Inner Pages <br /> to Suit Your Needs.
           </h2>
@@ -30,13 +36,19 @@ const InnerPage = () => {
                 .map((brand, index) => (
                   <div
                     key={index}
-                    className="mx-4 transition duration-300 ease-in-out hover:brightness-50 "
+                    className="mx-4 transition duration-300 ease-in-out hover:brightness-50"
                   >
-                    <img
-                      src={brand.logo}
-                      alt={`Brand ${index}`}
-                      className="w-[350px] h-[350px] rounded-sm"
-                    />
+                    <a
+                      href="https://vidion-template.webflow.io/blog/the-essentials-of-a-successful-video-production"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={brand.logo}
+                        alt={`Brand ${index}`}
+                        className="w-[350px] h-[350px] rounded-sm"
+                      />
+                    </a>
                   </div>
                 ))}
             </div>
